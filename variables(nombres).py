ce42011aef44bd7a4ada3a4349147d0ec09977c2
Deubilko <130190420+Deubilko@users.noreply.github.com>
2023-07-02 13:12:10 -0300

    Add files via upload

---
+++ variables(nombres).py
@@ -0,0 +1,14 @@
+#las variables son espacios en el programa donde se almacenan datos
+#algo como si fueran "cajas"
+
+#aca declare una variable llamada "nombre", la cual luego le asigne el dato "alan"
+nombre = "alan"
+#y con esto mande a imprimir por pantalla la bariable de nombre (el cual va a siempre mostrar el contenido) 
+print(nombre)
+
+#las variables se pueden redefinir a "mitad de camino" definiendolas otra vez con una nueva linea
+nombre2 = "diego"
+#ej: redinir la variable nombre2 con el nombre "martin" (cuando mandemos a imprimir tiene que decir martin)
+nombre2 = "martin"
+
+print(nombre2)
---
+++ variables(num).py
@@ -0,0 +1,19 @@
+#no hace falta aclarar que no se pueden sumar nombres 
+#las variables declaradas con valores numeros se manejan de la misma forma que las que tienen caracteres
+#ejemplos de la forma basica de sumar algo en pythom
+
+numero = 24 + 76
+print(numero)
+
+#aunque hay una manera mas eficiente, utilizando la variable junto a caracteres especificos
+#pueden simplificar muchisimo la suma 
+
+numero1 = 24
+numero1 += 76
+
+print(numero1)
+
+# += : significa suma
+# -= : significa resta
+# *= : significa multiplicacion
+# /= : significa division
